6fa5a16b3d9f3baca0d81ff664944df055d433f3
icing <pichada.chal@dome.tu.ac.th>
2024-11-28 00:46:57 +0700

    javascript.js-Function to update rating star

--- javascript.js
+++ javascript.js
@@ -23,3 +23,17 @@ guestForm.addEventListener('submit', function (e) {
 //to access the star//
 let stars = document.getElementsByClassName("star");
 let output = document.getElementById("output");
+
+// Funtion to update rating//
+function gfg(n) {
+    remove();
+    for (let i = 0; i < n; i++) {
+        if (n == 1) cls = "one";
+        else if (n == 2) cls = "two";
+        else if (n == 3) cls = "three";
+        else if (n == 4) cls = "four";
+        else if (n == 5) cls = "five";
+        stars[i].className = "star " + cls;
+    }
+    output.innerText = "Rating is: " + n + "/5";
+}

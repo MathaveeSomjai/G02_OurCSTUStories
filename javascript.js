37ab6a1f4951beecc44dbcff24cd3b0eb91bfc60
icing <pichada.chal@dome.tu.ac.th>
2024-11-28 00:40:02 +0700

    javascript.js-Declare function name e and variable in function e

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 //start guestbook//
 const guestForm = document.getElementById("guestform");//declare variable//
 const guestList = document.getElementById("guestlist");//declare variable//
+
+guestForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const Email = document.getElementById("email").value;//ประกาศตัวแปรและรับค่าเข้ามา//
+    const Name = document.getElementById("name").value;//ประกาศตัวแปรและรับค่าเข้ามา//
+    const Comment = document.getElementById("comment").value;//ประกาศตัวแปรและรับค่าเข้ามา//
+    
+    
+});
